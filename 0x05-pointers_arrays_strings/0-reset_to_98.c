2ea569500ee4570353889aa62ce0aa02762bd6fc
hanafi2023 <hanafisafiya78@gmail.com>
2023-03-27 13:58:21 -0700

    pointer to int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-reset_to_98.c
+ * Author: Safiya Hanafi
+ */
+
+#include "main.h"
+/**
+ * reset_to_98 - Function that takes a pointer to an int
+ * @n: Input
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Pri
