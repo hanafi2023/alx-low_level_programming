7f8143a2a2a1f7ebf78ee51b6b404145a2cb854c
hanafi2023 <hanafisafiya78@gmail.com>
2023-03-30 14:42:42 -0700

    encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+/*
+ * File: 7-leet.c
+ * Author: Queensly Udongwo
+ */
+
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337.
+ * @rep: inputn  string
+ * Return: string replace to 1337
+ */
+
+char *leet(char *rep)
+{
+	int ind1 = 0, ind2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (rep[ind1])
+	{
+		for (ind2 = 0; ind2 <= 7; ind2++)
+		{
+			if (rep[ind1] == leet[ind2] ||
+			    rep[ind1] - 32 == leet[ind2])
+				rep[ind1] = ind2 + '0';
+		}
+
+		ind1++;
+
+	}
+
+	return (rep);
+
+}
